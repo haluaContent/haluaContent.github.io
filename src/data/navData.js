ab4f0e85ebf343f288ae6e026f3ca84a5027edcf
Halua Content <haluaContent@gmail.com>
2024-04-23 01:20:39 +0600

    Logo Added

---
+++ src/data/navData.js
@@ -0,0 +1,21 @@
+const navData =  [
+
+ 
+    {
+        name: "Show Case",
+        path: "/showCase/"
+    },
+
+    {
+        name: "Clients",
+        path: "/clients/"
+    },
+     
+    {
+        name: "About",
+        path: "/aboutUs/"
+    },
+     
+]
+
+export default navData
