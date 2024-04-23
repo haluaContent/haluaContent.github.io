aaf8131b3b4af50356bf3de5d4a26da732018e59
Halua Content <haluaContent@gmail.com>
2024-04-24 00:19:54 +0600

    ShowCase

---
+++ src/data/ShowCaseData.js
@@ -0,0 +1,34 @@
+const showcaseData = [
+    {
+        name: "Dhyani Nodi",
+        client: "Penoa",
+        url: "https://www.youtube.com/embed/fO4Crrd7Pk0?si=HI2V9M4rTe73agrW"
+    },
+
+    {
+        name: "Paharer Buke",
+        client: "Penoa",
+        url: "https://www.youtube.com/embed/JBTVAEdFOno?si=VsmqhWhExYy17jco"
+    },
+
+    {
+        name: "Kuhu Daker Hon",
+        client: "Penoa",
+        url: "https://www.youtube.com/embed/Ul1d5rfcf-M?si=OD6l20lrNYAuL2VS"
+    },
+
+    {
+        name: "Rater Santar",
+        client: "Penoa",
+        url: "https://www.youtube.com/embed/sXKao0PPwlU?si=QiEOC3mg9Ee4DEu7"
+    },
+
+    {
+        name: "E Ruher Tole",
+        client: "Penoa",
+        url: "https://www.youtube.com/embed/cByWHoo-2bw?si=X2rWYTwwVU6Hvsxi"
+    },
+]
+
+
+export default showcaseData
