f3167a27a91e034748a43a26a25c02ca1679122c
Halua Content <haluaContent@gmail.com>
2024-04-24 00:42:08 +0600

    Clients

---
+++ src/data/ClientData.js
@@ -0,0 +1,35 @@
+const clientData = [
+
+    {
+        name: "Penoa",
+    imageUrl: "../../public/clientImages/penoa.png",
+},
+
+    {
+        name: "Jaqat Calculator",
+    imageUrl: "../../public/clientImages/jc.png",
+},
+
+{
+    name: "Code Log",
+imageUrl: "../../public/clientImages/code_log.png",
+},
+
+{
+    name: "Far East Organics",
+imageUrl: "../../public/clientImages/feo.png",
+},
+
+{
+    name: "Sobai",
+imageUrl: "../../public/clientImages/sobai.png",
+},
+
+{
+    name: "Priyotoma",
+imageUrl: "../../public/clientImages/pt.png",
+},
+
+]
+
+export default clientData
