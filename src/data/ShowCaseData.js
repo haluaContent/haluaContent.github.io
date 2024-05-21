9075e98e23d4fba1f9403fdcc54856264312b7a5
Halua Content <haluaContent@gmail.com>
2024-05-21 09:10:53 +0600

    Fixed Padding

--- src/data/ShowCaseData.js
+++ src/data/ShowCaseData.js
@@ -2,31 +2,36 @@ const showcaseData = [
     {
         name: "Dhyani Nodi",
         client: "Penoa",
-        url: "https://www.youtube.com/embed/fO4Crrd7Pk0?si=HI2V9M4rTe73agrW"
+        url: "https://www.youtube.com/embed/fO4Crrd7Pk0?si=HI2V9M4rTe73agrW",
+        year: 2024
     },
 
     {
         name: "Paharer Buke",
         client: "Penoa",
-        url: "https://www.youtube.com/embed/JBTVAEdFOno?si=VsmqhWhExYy17jco"
+        url: "https://www.youtube.com/embed/JBTVAEdFOno?si=VsmqhWhExYy17jco",
+        year: 2024
     },
 
     {
         name: "Kuhu Daker Hon",
         client: "Penoa",
-        url: "https://www.youtube.com/embed/Ul1d5rfcf-M?si=OD6l20lrNYAuL2VS"
+        url: "https://www.youtube.com/embed/Ul1d5rfcf-M?si=OD6l20lrNYAuL2VS",
+        year: 2024
     },
 
     {
         name: "Rater Santar",
         client: "Penoa",
-        url: "https://www.youtube.com/embed/sXKao0PPwlU?si=QiEOC3mg9Ee4DEu7"
+        url: "https://www.youtube.com/embed/sXKao0PPwlU?si=QiEOC3mg9Ee4DEu7",
+        year: 2024
     },
 
     {
         name: "E Ruher Tole",
         client: "Penoa",
-        url: "https://www.youtube.com/embed/cByWHoo-2bw?si=X2rWYTwwVU6Hvsxi"
+        url: "https://www.youtube.com/embed/cByWHoo-2bw?si=X2rWYTwwVU6Hvsxi",
+        year: 2024
     },
 ]
 
